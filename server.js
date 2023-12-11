f631056e45d9a78a68010ef2d1c37df63ba85d32
Phil St Croix <127119752+PhilStCroix@users.noreply.github.com>
2023-12-11 11:32:20 -0330

    test of server and index.ejs with sql data
    Co-authored-by: saamhynes <saamhynes@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const dal = require('./src/dal');
+const apiRouter = require('./src/api');
+const { getAllAuthors, getAllGenres } = require('./src/dal');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use('/api', apiRouter);
+
+app.get('/', async (req, res) => {
+  console.log('Fetching all books');
+  try {
+    const books = await dal.getAllBooks();
+    res.render('index.ejs', { books });
+  } catch (error) {
+    // Handle error gracefully
+    console.error('Error fetching books:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/books/add', async (req, res) => {
+  console.log('Fetching all books2');
+  try {
+    const authors = await getAllAuthors();
+    const genres = await getAllGenres();
+    res.render('add.ejs', { authors, genres });
+  } catch (error) {
+    console.error('Error fetching authors and genres:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/books', async (req, res) => {
+  console.log('Fetching all books3');
+  const { title, author_id, genre_id, publication_year, isbn } = req.body;
+  try {
+    await dal.createBook(title, author_id, genre_id, publication_year, isbn);
+    res.redirect('/');
+  } catch (error) {
+    // Handle error gracefully
+    console.error('Error creating book:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+  
+app.get('/books/:id', async (req, res) => {
+  console.log('Fetching all books4');
+  const bookId = req.params.id;
+  try {
+    console.log('Fetching book details for ID:', bookId);
+    const book = await dal.getBookById(bookId);
+    console.log('Retrieved book:', book);
+    if (!book) {
+      console.log('Book not found');
+      res.status(404).send('Book not found');
+      return;
+    }
+    res.render('show.ejs', { book });
+  } catch (error) {
+    console.error('Error fetching book details:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/books/:id/edit', async (req, res) => {
+  console.log('Fetching all books5');
+  const bookId = req.params.id;
+  try {
+    console.log('Fetching book for editing with ID:', bookId);
+    const book = await dal.getBookById(bookId);
+    console.log('Retrieved book for editing:', book);
+    if (!book) {
+      console.log('Book not found for editing');
+      res.status(404).send('Book not found');
+      return;
+    }
+    res.render('edit.ejs', { book });
+  } catch (error) {
+    console.error('Error fetching book for editing:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/books', async (req, res) => {
+  console.log('Fetching all books6');
+  const { title, author_id, genre_id, publication_year, isbn } = req.body;
+  try {
+    await dal.createBook(title, author_id, genre_id, publication_year, isbn);
+    res.redirect('/');
+  } catch (error) {
+    // Handle error gracefully
+    console.error('Error creating book:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.put('/books/:id', async (req, res) => {
+  console.log('Fetching all books7');
+  const bookId = req.params.id;
+  const { title, author_id, genre_id, publication_year, isbn } = req.body;
+  try {
+    await dal.updateBook(bookId, title, author_id, genre_id, publication_year, isbn);
+    res.redirect('/');
+  } catch (error) {
+    // Handle error gracefully
+    console.error('Error updating book:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.patch('/books/:id', async (req, res) => {
+  console.log('Fetching all books8');
+  const bookId = req.params.id;
+  const { title, author_id, genre_id, publication_year, isbn } = req.body;
+  try {
+    await dal.partiallyUpdateBook(bookId, title, author_id, genre_id, publication_year, isbn);
+    res.redirect('/');
+  } catch (error) {
+    // Handle error gracefully
+    console.error('Error partially updating book:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.delete('/books/:id', async (req, res) => {
+  console.log('Fetching all books9');
+  const bookId = req.params.id;
+  try {
+    await dal.deleteBook(bookId);
+    res.redirect('/');
+  } catch (error) {
+    // Handle error gracefully
+    console.error('Error deleting book:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/dal.js
@@ -0,0 +1,92 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'FST3',
+  password: 'Olivia2021',
+  port: 5432,
+});
+
+async function getAllBooks() {
+  const result = await pool.query('SELECT * FROM books ORDER BY book_id ASC');
+  return result.rows;
+}
+
+async function getBookById(bookId) {
+  // Convert bookId to an integer, or return null if it's not a valid integer
+  const bookIdAsInt = parseInt(bookId, 10);
+  if (isNaN(bookIdAsInt)) {
+    return null; // or you can throw an error if you prefer
+  }
+
+  // Perform a database query to get the book with the specified ID
+  const result = await pool.query('SELECT * FROM books WHERE book_id = $1', [bookIdAsInt]);
+
+  // Check if a book was found
+  if (result.rows.length === 0) {
+    return null; // or you can throw an error if you prefer
+  }
+
+  // Return the retrieved book
+  return result.rows[0];
+}
+
+async function createBook(title, author_id, genre_id, publication_year, isbn) {
+  await pool.query(
+    'INSERT INTO books (title, author_id, genre_id, publication_year, isbn) VALUES ($1, $2, $3, $4, $5)',
+    [title, author_id, genre_id, publication_year, isbn]
+  );
+}
+
+async function updateBook(bookId, title, author_id, genre_id, publication_year, isbn) {
+  await pool.query(
+    'UPDATE books SET title = $1, author_id = $2, genre_id = $3, publication_year = $4, isbn = $5 WHERE book_id = $6',
+    [title, author_id, genre_id, publication_year, isbn, bookId]
+  );
+}
+
+async function partiallyUpdateBook(bookId, title, author_id, genre_id, publication_year, isbn) {
+  await pool.query(
+    'UPDATE books SET title = COALESCE($1, title), author_id = COALESCE($2, author_id), genre_id = COALESCE($3, genre_id), publication_year = COALESCE($4, publication_year), isbn = COALESCE($5, isbn) WHERE book_id = $6',
+    [title, author_id, genre_id, publication_year, isbn, bookId]
+  );
+}
+
+async function getAllAuthors() {
+  const result = await pool.query('SELECT * FROM authors ORDER BY author_id ASC');
+  return result.rows;
+}
+
+async function getAllGenres() {
+  const result = await pool.query('SELECT * FROM genres ORDER BY genre_id ASC');
+  return result.rows;
+}
+
+async function authorExists(authorId) {
+  const result = await pool.query('SELECT * FROM authors WHERE author_id = $1', [authorId]);
+  return result.rows.length > 0;
+}
+
+async function genreExists(genreId) {
+  const result = await pool.query('SELECT * FROM genres WHERE genre_id = $1', [genreId]);
+  return result.rows.length > 0;
+}
+
+
+async function deleteBook(bookId) {
+  await pool.query('DELETE FROM books WHERE book_id = $1', [bookId]);
+}
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  partiallyUpdateBook,
+  deleteBook,
+  getAllAuthors,
+  getAllGenres,
+  authorExists,
+  genreExists,
+};
